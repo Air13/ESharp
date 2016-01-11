9f1ae5733e094f20ea11723c8ef7b5b9b0b7b495
Air13 <dukenukemail@gmail.com>
2016-01-11 21:07:39 +0300

    MpHp Air13
    bars+digits

---
+++ MpHp Air13/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ManaHpBars
+{
+    using System;
+    using System.Linq;
+
+    using Ensage;
+    using Ensage.Common;
+
+    using SharpDX;
+	
+
+
+
+
+    internal class Program
+    {
+        #region Methods
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (!Game.IsInGame)
+            {
+                return;
+            }
+            var me = ObjectMgr.LocalPlayer;
+            if (me == null || me.Team == Team.Observer)
+            {
+                return;
+            }
+
+            var enemies = ObjectMgr.GetEntities<Hero>().Where(x => x.IsVisible && x.IsAlive && x.MaximumMana > 0 && !x.IsIllusion && x.Team != me.Team).ToList();
+            foreach (var enemy in enemies)
+            {
+
+                var start = HUDInfo.GetHPbarPosition(enemy) + new Vector2(0, HUDInfo.GetHpBarSizeY(enemy) + 1);
+                var mprc = enemy.Mana / enemy.MaximumMana;
+                var size = new Vector2(HUDInfo.GetHPBarSizeX(enemy), HUDInfo.GetHpBarSizeY(enemy) / 2);
+                Drawing.DrawRect(start, size + new Vector2(1, 1), new Color(0, 0, 50, 150));
+                Drawing.DrawRect(start, new Vector2(size.X * mprc, size.Y), Color.RoyalBlue);
+                Drawing.DrawRect(start + new Vector2(-1, -1), size + new Vector2(3, 3), Color.Black, true);
+                var text = string.Format("{0}", (int)enemy.Mana);
+				var text2 = string.Format("{0}", (int)enemy.Health);
+                var textSize = Drawing.MeasureText(text, "segoeui", new Vector2(size.Y * 2, size.X), FontFlags.AntiAlias);
+				var textSize2 = Drawing.MeasureText(text2, "segoeui", new Vector2(size.Y * 2, size.X), FontFlags.AntiAlias);
+                var textPos = start + new Vector2(size.X / 2 - textSize.X / 2, -textSize.Y / 2 + 13);
+				var textPos2 = start + new Vector2(size.X / 2 - textSize2.X / 2, -textSize2.Y / 2 - 25);
+                Drawing.DrawText(text,textPos,new Vector2(size.Y * 7/2 , size.X * 5/2),Color.White,FontFlags.AntiAlias | FontFlags.DropShadow);
+				Drawing.DrawText(text2,textPos2,new Vector2(size.Y * 7/2 , size.X * 5/2),Color.White,FontFlags.AntiAlias | FontFlags.DropShadow);
+			}
+        }
+
+        private static void Main()
+        {
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        #endregion
+    }
+}
